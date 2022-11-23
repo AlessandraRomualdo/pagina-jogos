e778f1c73489768a49a7f99410c562e7cc43819d
Alessandra Romualdo <106454369+AlessandraRomualdo@users.noreply.github.com>
2022-11-23 16:38:12 -0300

    funcao de mostrar artigo ainda não funciona

--- script/funcoes.js
+++ script/funcoes.js
@@ -1,9 +1,18 @@
 
 
 const clickMenu =()=>{
-    if(menu.style.display == 'none'){
+    if(menu.style.display == 'block'){
+        menu.style.display = 'none'
+    }else{
         menu.style.display = 'block'
+    }
+}
+const mudouTamanho = () =>{
+    if(window.innerWidth >= 768){
+        menu.style.display = 'block'
+        burguer.style.display = 'none'
     }else{
         menu.style.display = 'none'
+        burguer.style.display = 'block'
     }
 }
